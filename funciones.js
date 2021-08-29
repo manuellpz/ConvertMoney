c66dd1a7cd37f41186de33d189d67f35fa5849ce
ManuelLpz <manuel@mztweb.com>
2021-08-29 17:16:15 -0600

    Funcionalidad Pt1

--- funciones.js
+++ funciones.js
@@ -1,36 +1,99 @@
-document.getElementById("convertir").addEventListener("click",function(){
-    const url = 'https://open.exchangerate-api.com/v6/latest';
+const firstValue = document.getElementById('txtFirstValue')
+const secondValue = document.getElementById('txtSecondValue')
+const firstOption = document.getElementById('firstOption')
+const secondOption = document.getElementById('secondOption')
+const url = 'https://open.exchangerate-api.com/v6/latest';
 
-    let cantidad = parseInt(document.getElementById("cantidad").value);
-    let moneda = document.getElementById("opciones").value;
-    let resultado = document.getElementById("resultado");
+firstValue.addEventListener('input',(e) => {
+    let result = 0
 
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>{
-        if(moneda == "mxyen")
+    .then(response => response.json())
+    .then(data => {
+        const currencies = data.rates
+        if(firstOption.value == 'USD' && secondOption.value == 'MXN')
         {
-            resultado.value = (cantidad * (data.rates.JPY/data.rates.MXN)).toFixed(2) + " Yenes";
+            result = (e.target.value * currencies.MXN).toFixed(2)
         }
-        else if(moneda == "yenmx")
-        {
-            resultado.value = (cantidad * (data.rates.MXN/data.rates.JPY)).toFixed(2) + " Pesos"; 
-        }
-        else if(moneda == "mxdolar")
-        {
-            resultado.value = (cantidad * (data.rates.USD/data.rates.MXN)).toFixed(2) + " Dolares"; 
-        }
-        else if(moneda == "dolarmx")
-        {
-            resultado.value = (cantidad * (data.rates.MXN/data.rates.USD)).toFixed(2) + " Pesos"; 
-        }
-        else if(moneda == "mxwon")
-        {
-            resultado.value = (cantidad * (data.rates.KRW/data.rates.MXN)).toFixed(2) + " Wones"; 
-        }
-        else if(moneda == "wonmx")
-        {
-            resultado.value = (cantidad * (data.rates.MXN/data.rates.KRW)).toFixed(2) + " Pesos"; 
-        }
-    }).catch(error=>alert(error));
-});
+
+
+        secondValue.value = result;
+    })
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("convertir").addEventListener("click",function(){
+//     const url = 'https://open.exchangerate-api.com/v6/latest';
+
+//     let cantidad = parseInt(document.getElementById("cantidad").value);
+//     let moneda = document.getElementById("opciones").value;
+//     let resultado = document.getElementById("resultado");
+
+//     fetch(url)
+//     .then(response=>response.json())
+//     .then(data=>{
+//         if(moneda == "mxyen")
+//         {
+//             resultado.value = (cantidad * (data.rates.JPY/data.rates.MXN)).toFixed(2) + " Yenes";
+//         }
+//         else if(moneda == "yenmx")
+//         {
+//             resultado.value = (cantidad * (data.rates.MXN/data.rates.JPY)).toFixed(2) + " Pesos"; 
+//         }
+//         else if(moneda == "mxdolar")
+//         {
+//             resultado.value = (cantidad * (data.rates.USD/data.rates.MXN)).toFixed(2) + " Dolares"; 
+//         }
+//         else if(moneda == "dolarmx")
+//         {
+//             resultado.value = (cantidad * (data.rates.MXN/data.rates.USD)).toFixed(2) + " Pesos"; 
+//         }
+//         else if(moneda == "mxwon")
+//         {
+//             resultado.value = (cantidad * (data.rates.KRW/data.rates.MXN)).toFixed(2) + " Wones"; 
+//         }
+//         else if(moneda == "wonmx")
+//         {
+//             resultado.value = (cantidad * (data.rates.MXN/data.rates.KRW)).toFixed(2) + " Pesos"; 
+//         }
+//     }).catch(error=>alert(error));
+// });
